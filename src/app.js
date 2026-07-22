f0b4fdb7446906c95a6f977c42c41fa815b60eb2
johan <relativitydev3@gmail.com>
2026-07-22 09:18:43 -0500

    views

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ const indexRoutes = require('./routes/index');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../public/views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/css', express.static(path.join(__dirname, '../public/css')));
+app.use('/js', express.static(path.join(__dirname, '../public/js')));
+app.use('/img', express.static(path.join(__dirname, '../public/img')));
 
 app.use('/', indexRoutes);
 
