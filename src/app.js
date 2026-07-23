973a049bd2f08db47f8524d21784a4876cb82e21
johan <relativitydev3@gmail.com>
2026-07-23 12:22:58 -0500

    portafolio pdf

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/css', express.static(path.join(__dirname, '../public/css')));
 app.use('/js', express.static(path.join(__dirname, '../public/js')));
 app.use('/img', express.static(path.join(__dirname, '../public/img')));
+app.use('/doc', express.static(path.join(__dirname, '../public/doc')));
 
 app.use('/', indexRoutes);
 
